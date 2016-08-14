d5d578b894fda46d0a00c8acb2e34014580b6d96
Ernesto Menéndez <pyalec@gmail.com>
2016-08-13 23:22:02 -0600

    implement draw function for Snake struct and allow defining the color of the snake

--- src/main.rs
+++ src/main.rs
@@ -53,9 +53,16 @@ impl Dir {
 struct Snake {
     head:  Pos,
     moves: Vec<Dir>,
+    color: types::Color,
 }
 
 impl Snake {
+    fn draw<G>(&self, transform: math::Matrix2d, g: &mut G) where G: Graphics {
+        for rect in self.to_body_rectangles() {
+            rectangle(self.color, rect, transform, g);
+        }
+    }
+
     fn to_body_rectangles(&self) -> Vec<[f64; 4]> {
         self.to_body_positions()
             .iter()
@@ -88,17 +95,15 @@ fn main() {
 
     let snake = Snake {
         head: Pos { x: 20, y: 20 },
-        moves: vec![Dir::Up, Dir::Up, Dir::Left, Dir::Left, Dir::Left, Dir::Left]
+        moves: vec![Dir::Up, Dir::Up, Dir::Left, Dir::Left, Dir::Left, Dir::Left],
+        color: [1.0, 0.0, 1.0, 1.0]
     };
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
             clear([0.0; 4], g);
 
-            for rect in snake.to_body_rectangles() {
-                rectangle([1.0, 0.0, 0.0, 1.0], // red
-                          rect, c.transform, g);
-            }
+            snake.draw(c.transform, g);
         });
     }
 }
